823873e5c476ddfaf7970ad3d7049c570ecd36ca
Joseph Kogut <joseph@balena.io>
2025-01-31 10:05:58 +0000

    Initial wayland implementation
    This implements a wayland compositor with Xwayland for compatibility
    with apps that require X, seatd for handling DRM master and input
    devices, and pulse for audio.
    
    Signed-off-by: Joseph Kogut <joseph@balena.io>

--- steam/entry.sh
+++ steam/entry.sh
@@ -1,39 +1,21 @@
 #!/bin/bash
-setup_devtmpfs() {
-	newdev=/tmp/dev
-	mkdir -p "$newdev"
-	mount -t devtmpfs none "$newdev"
-	mount --move /dev/console "$newdev/console"
-	mount --move /dev/mqueue "$newdev/mqueue"
-	mount --move /dev/pts "$newdev/pts"
-	mount --move /dev/shm "$newdev/shm"
-	umount /dev
-	mount --move "$newdev" /dev
-	ln -sf /dev/pts/ptmx /dev/ptmx
-}
+set -e
 
-setup_binfmt_handlers() {
-	BINFMT_MISC_FS=/proc/sys/fs/binfmt_misc
-	if [ -f /etc/binfmt.d/box86.conf ]; then
-		if ! mountpoint $BINFMT_MISC_FS; then
-			mount -t binfmt_misc none $BINFMT_MISC_FS
-		else
-			echo "binfmt_misc already mounted"
-		fi
+mkdir -p /var/run/nscd
+nscd
 
-		if [ ! -f $BINFMT_MISC_FS/x86 ]; then
-			grep -v '^#' /etc/binfmt.d/box86.conf > $BINFMT_MISC_FS/register
-		else
-			echo "x86 handler already registered"
-		fi
-	fi
-}
+while [ "$(cat /proc/sys/user/max_user_namespaces)" -lt 100 ]; do sleep 0.1; done
 
-setup_devtmpfs
-setup_binfmt_handlers
+printf "%s" "Waiting for pulseaudio..."
+su steamuser -c "while ! pactl info; do sleep 0.1; done"
+printf "%s\n" "done"
 
-while ! xset -q; do
-	sleep 1;
-done
+su steamuser -c "pactl set-sink-mute 0 0"
+su steamuser -c "pactl set-sink-volume 0 100%"
 
-su steamuser -c 'STEAMOS=1 STEAM_RUNTIME=1 /usr/bin/steam -tcp +open steam://open/minigameslist'
+printf "%s" "Waiting for display server..."
+while ! su steamuser -c "xset -q >/dev/null"; do sleep 0.5; done
+printf "%s\n" "done"
+
+su steamuser -c 'STEAMOS=1 STEAM_RUNTIME=1 /usr/bin/steam -gamepadui'
+
